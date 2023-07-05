ba5207ac1c1bef1721e524e7fade9e7398e3a3d1
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-07-05 06:54:43 +0530

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Rishav Gupta on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
